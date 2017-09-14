232dc6a7c1d96596087ffb1c259f75ac36bba76b
Linh Tran Tuan <linhduonggnu@gmail.com>
2017-09-14 21:53:27 +0700

    Try to fix incompatible of tty console in travis run

--- tty_test.go
+++ tty_test.go
@@ -12,23 +12,29 @@ func TestTTY(t *testing.T) {
 
 func testTTY(t *testing.T, loop *UvLoop) {
 	go func() {
+		defer func() {
+			if e := recover(); e != nil {
+				fmt.Println(e)
+			}
+		}()
+
 		tty, err := UvTTYInit(loop, 1, 1, nil) // stdout with readable
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		// if r := tty.SetMode(UV_TTY_MODE_NORMAL); r != 0 {
-		// 	t.Fatal(ParseUvErr(r))
-		// }
+		fmt.Println(tty.GetWinsize())
+
+		if r := tty.SetMode(UV_TTY_MODE_NORMAL); r != 0 {
+			t.Fatal(ParseUvErr(r))
+		}
 
 		sampleTTY(tty)
 
-		fmt.Println(tty.GetWinsize())
-
 		time.Sleep(2 * time.Second)
 
-		// tty.ResetMode()
+		tty.ResetMode()
 
-		// tty.Freemem()
+		tty.Freemem()
 	}()
 }
